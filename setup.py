2aa78a622486429d159738c2ffc7ba918b9dc156
Boris <564773807@qq.com>
2023-03-01 10:11:33 +0800

    version 1.4

--- setup.py
+++ setup.py
@@ -11,12 +11,18 @@ Created on 2022/8/4 11:38 上午
 from setuptools import setup, find_packages
 
 setup(
+    version="1.4",
     name="asst",
-    version="1.3",
+    license="MIT",
+    author="Boris",
+    author_email="boris_liu@foxmail.com",
     packages=find_packages(),
     install_requires=["openai"],
     entry_points="""
         [console_scripts]
         asst=chatgpt.main:main
     """,
+    description="ChatGPT shell command",
+    url="https://github.com/Boris-code/chatgpt-cli.git",
+    classifiers=["Programming Language :: Python :: 3"],
 )
